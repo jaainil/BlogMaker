b261be409cbface389b63b066c7a9c9bcdc734cb
Jainil Prajapati 🪐 <github-jainil@1209.in>
2024-11-26 00:57:35 +0530

    Enhance layout and article rendering components: add suppressHydrationWarning to RootLayout, update ThemeProvider for cleaner props handling, and include HorizontalRule in RenderArticle imports for improved article formatting.

--- app/components/dashboard/RenderArticle.tsx
+++ app/components/dashboard/RenderArticle.tsx
@@ -14,8 +14,9 @@ import BlockQuote from "@tiptap/extension-blockquote";
 import TextStyle from "@tiptap/extension-text-style";
 import CodeBlock from "@tiptap/extension-code-block";
 import OrderList from "@tiptap/extension-ordered-list";
-import Bold from "@tiptap/extension-bold"; // Ajoutez cette ligne
-import HardBreak from "@tiptap/extension-hard-break"; // Ajoutez cette ligne
+import Bold from "@tiptap/extension-bold";
+import HardBreak from "@tiptap/extension-hard-break";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
 
 export function RenderArticle({ json }: { json: JSONContent }) {
   const outPut = useMemo(() => {
@@ -33,8 +34,9 @@ export function RenderArticle({ json }: { json: JSONContent }) {
       TextStyle,
       CodeBlock,
       OrderList,
-      Bold, // Ajoutez cette ligne
-      HardBreak, // Ajoutez cette ligne
+      Bold,
+      HardBreak,
+      HorizontalRule,
     ]);
   }, [json]);
 
