67b1bc7f155bb2c11f6fa218280faf8fee6b69ae
Jainil Prajapati 🪐 <github-jainil@1209.in>
2024-11-25 18:55:15 +0530

    Refactor DeletePage component in page.tsx: update PageProps to use Promises for params and searchParams, enhancing async handling and improving clarity in data retrieval.

--- app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
+++ app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
@@ -10,11 +10,17 @@ import {
 } from "@/components/ui/card";
 import Link from "next/link";
 
-export default function DeleteForm({
-  params,
-}: {
-  params: { siteId: string; articleId: string };
-}) {
+interface PageProps {
+  params: Promise<{
+    siteId: string;
+    articleId: string;
+  }>;
+  searchParams?: Promise<Record<string, string | string[] | undefined>>;
+}
+
+export default async function DeletePage({ params, searchParams }: PageProps) {
+  const resolvedParams = await params;
+
   return (
     <div className="flex flex-1 items-center justify-center">
       <Card className="max-w-xl">
@@ -27,11 +33,17 @@ export default function DeleteForm({
         </CardHeader>
         <CardFooter className="w-full flex justify-between">
           <Button variant="secondary" asChild>
-            <Link href={`/dashboard/sites/${params.siteId}`}>Cancel</Link>
+            <Link href={`/dashboard/sites/${resolvedParams.siteId}`}>
+              Cancel
+            </Link>
           </Button>
           <form action={DeletePost}>
-            <input type="hidden" name="articleId" value={params.articleId} />
-            <input type="hidden" name="siteId" value={params.siteId} />
+            <input
+              type="hidden"
+              name="articleId"
+              value={resolvedParams.articleId}
+            />
+            <input type="hidden" name="siteId" value={resolvedParams.siteId} />
             <SubmitButton variant="destructive" text="Delete Article" />
           </form>
         </CardFooter>
